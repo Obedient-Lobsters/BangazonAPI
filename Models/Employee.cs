e8fe4262610cef2ff13f57458b95a30a7cd4c95b
Jordan Williams <jordanwilliams288@gmail.com>
2018-09-12 11:58:18 -0500

    models for Employee and Computer

---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; } // ? means that the variable can be null
+        public bool Supervisor { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
