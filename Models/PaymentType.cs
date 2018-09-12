1ba22fd61f368f208c96a9882187dc5b6ce9b33f
Shu52 <shusajid@gmail.com>
2018-09-12 15:34:36 -0500

    Created PaymentType.cs Model for #3

---
+++ Models/PaymentType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BangazonAPI.Models
+{   
+    //    Structure in Database
+    //    CREATE TABLE PaymentType(
+    //    PaymentTypeId INTEGER NOT NULL PRIMARY KEY,
+    //    PaymentTypeName varchar(15) NOT NULL
+    //);
+    public class PaymentType
+    {
+        [Key]
+        public int PaymentTypeId { get; set; }
+        [StringLength(15, ErrorMessage = "Maxinum lenght is 15 characters"),Required]
+        public string PaymentTypeName { get; set; }
+    }
+}
