7b70068a6b112a707a4758168dc4f6304664be0c
Shu52 <shusajid@gmail.com>
2018-09-12 16:08:39 -0500

    Fixed spelling errors, removed comment block

--- Models/PaymentType.cs
+++ Models/PaymentType.cs
@@ -3,16 +3,11 @@
 
 namespace BangazonAPI.Models
 {   
-    //    Structure in Database
-    //    CREATE TABLE PaymentType(
-    //    PaymentTypeId INTEGER NOT NULL PRIMARY KEY,
-    //    PaymentTypeName varchar(15) NOT NULL
-    //);
     public class PaymentType
     {
         [Key]
         public int PaymentTypeId { get; set; }
-        [StringLength(15, ErrorMessage = "Maxinum lenght is 15 characters"),Required]
+        [StringLength(15, ErrorMessage = "Maximum length is 15 characters"),Required]
         public string PaymentTypeName { get; set; }
     }
 }
