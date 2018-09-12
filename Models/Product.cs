27d61822cffdefaace2c5ce325dfb2c833634f37
Joey <smithjoey5123@gmail.com>
2018-09-12 14:43:47 -0500

    Employee, Product, and TrainingProgram Models

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int ProductTypeId { get; set; }
+        public ProductType ProductType { get; set; }
+    }
+}
---
+++ Models/TrainingProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BangazonAPI.Models
+{
+    public class TrainingProgram
+    {
+        [Key]
+        public int TrainingProgramId { get; set; }
+
+        [Required]
+        public string ProgramName { get; set; }
+        [StringLength(80,
+            ErrorMessage ="Too Many Characters")]
+        public string StartDate { get; set; }
+        [StringLength(10,
+            ErrorMessage ="Please Enter A Valid Start Date")]
+        public string EndDate { get; set; }
+        [StringLength(10,
+            ErrorMessage = "Please Enter A Valid End Date")]
+        public int MaximumAttendees { get; set; }
+    }
+}
