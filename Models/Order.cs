3a364ed22d9fee536f3cdf83a948af0753691340
williamkimball <36681575+williamkimball@users.noreply.github.com>
2018-09-12 14:45:46 -0500

    added models directory and created order and department models

---
+++ Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int? PaymentTypeId { get; set; } // ? means that the variable can be null
+        public int CustomerId { get; set; }
+
+    }
+}
