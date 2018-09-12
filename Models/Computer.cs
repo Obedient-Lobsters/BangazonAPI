9cda2d4ac0e56637928e5809cf8f3188ea6c8058
Jordan Williams <jordanwilliams288@gmail.com>
2018-09-12 15:42:44 -0500

    added Computer model

---
+++ Models/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Computer
+    {
+        [Key]
+        public int ComputerId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DatePurchased { get; set; }
+        public DateTime DateDecommissioned { get; set; }
+
+        [Required]
+        public bool Working { get; set; }
+        public string ModelName { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
