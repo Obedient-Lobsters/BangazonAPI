c6b1b0d1f9f8a1e0395844608186bddb0ff68492
williamkimball <36681575+williamkimball@users.noreply.github.com>
2018-09-12 22:25:33 -0500

    created model for the ProductOrder Join table

--- Models/ProductOrder.cs
+++ Models/ProductOrder.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BangazonAPI.Models
 {
-    public class Class
+    public class ProductOrder
     {
+        [Key]
+        public int ProductOrderId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+
     }
 }
