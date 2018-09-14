f7271867684b8df2b19e358953870a9d0ea3f736
Leah <leahgwin@gmail.com>
2018-09-14 11:58:24 -0500

    Added the GET for ProductType

---
+++ Controllers/ProductTypeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class ProductTypeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ProductTypeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/producttype
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string sql = "SELECT * FROM ProductType";
+
+                var fullProductType = await conn.QueryAsync<ProductType>(
+                    sql);
+                return Ok(fullProductType);
+            }
+
+        }
+
+   
+    }
+}
