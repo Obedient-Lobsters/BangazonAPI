719fdd1af2def2aeb40a5662ac490b83acb6d720
Shu52 <shusajid@gmail.com>
2018-09-14 18:45:46 -0500

    Completed Delete Method

--- Controllers/PaymentTypeController.cs
+++ Controllers/PaymentTypeController.cs
@@ -21,11 +21,14 @@ namespace BangazonAPI.Controllers
     {
         private readonly IConfiguration _config;
 
+        // This method... I have no idea how this method works. 
+        // I just know that it takes the private _config and gives it the config value 
+        // so it can work in the Connection method
         public PaymentTypeController(IConfiguration config)
         {
             _config = config;
         }
-
+        // This method makes the connection to database
         public IDbConnection Connection
         {
             get
@@ -34,6 +37,7 @@ namespace BangazonAPI.Controllers
             }
         }
         // GET: All PaymentType
+        //This method executes GET command and does require a parameter
         [HttpGet]
         public async Task<IActionResult>Get()
         {
@@ -46,6 +50,7 @@ namespace BangazonAPI.Controllers
         }
 
         // GET: PaymentType/3
+        // The Method executes GET one item from database. It's parameter is id taken from route. It is an overload Get method
         [HttpGet("{id}", Name = "GetPaymentType")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
@@ -60,6 +65,7 @@ namespace BangazonAPI.Controllers
 
 
         // POST: PaymentType/Post
+        // This method executes POST command. It's parameter is body from model
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] PaymentType paymentType)
         {
@@ -77,6 +83,7 @@ namespace BangazonAPI.Controllers
             }
         }
         // PUT PaymentType/Put
+        // This Method excutes the PUT method. It's parameters is id from route, and body from model
         [HttpPut("{id}")]
         public async Task<IActionResult> Put([FromRoute] int id, [FromBody] PaymentType paymentType)
         {
@@ -109,6 +116,7 @@ namespace BangazonAPI.Controllers
                 }
             }
         }
+        //This method checks if item exists in database returns a boolean value. This method is refered to in PUT method
         private bool PaymentTypeExists(int id)
         {
             string sql = $"SELECT PaymentTypeId, PaymentTypeName FROM PaymentType WHERE paymentTypeId = {id}";
@@ -119,50 +127,23 @@ namespace BangazonAPI.Controllers
         }
 
 
-        //// GET: PaymentType/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
+        // DELETE PaymentType/Delete
+        // This method executes the DELETE command. It parameter is id taken from route
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            string sql = $@"DELETE FROM PaymentType WHERE paymentTypeId = {id}";
 
-        //// POST: PaymentType/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add update logic here
+            using (IDbConnection conn = Connection)
+            {
+                int rowsAffected = await conn.ExecuteAsync(sql);
+                if (rowsAffected > 0)
+                {
+                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+                }
+                throw new Exception("No rows affected");
+            }
 
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: PaymentType/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: PaymentType/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add delete logic here
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+        }
     }
 }
