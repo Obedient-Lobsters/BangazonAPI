e2b53bf1c5aaf115f2c9694b397e06a823db2b46
Joey <smithjoey5123@gmail.com>
2018-09-14 11:50:46 -0500

    Postnow working in Products Controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿//Author: Joey Smith
+//Purpose: Controller for Products
+
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Data.SqlClient;
+using BangazonAPI.Models;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        
+        public ProductController (IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET All Products
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string sql = "SELECT * FROM Product";
+                var AllProducts = await conn.QueryAsync<Product>(sql);
+                return Ok(AllProducts);
+            }
+        }
+
+//        // GET api/values/5
+//        [HttpGet("{id}")]
+//        public ActionResult<string> Get(int id)
+//        {
+//            return "value";
+//        }
+
+//        // POST api/values
+//        [HttpPost]
+//        public void Post([FromBody] string value)
+//        {
+//        }
+
+//        // PUT api/values/5
+//        [HttpPut("{id}")]
+//        public void Put(int id, [FromBody] string value)
+//        {
+//        }
+
+//        // DELETE api/values/5
+//        [HttpDelete("{id}")]
+//        public void Delete(int id)
+//        {
+//        }
+  }
+}
