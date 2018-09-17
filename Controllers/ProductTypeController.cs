a7dba10db92ab2d317e2d510b57680386b42e16e
Leah <leahgwin@gmail.com>
2018-09-17 11:07:07 -0500

    added the PUT method and the boolean check needed for it

--- Controllers/ProductTypeController.cs
+++ Controllers/ProductTypeController.cs
@@ -59,7 +59,7 @@ namespace BangazonAPI.Controllers
         }
 
 
-        // POST /values
+        // POST producttype/POST
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] ProductType value)
         {
@@ -75,7 +75,49 @@ namespace BangazonAPI.Controllers
                 value.ProductTypeId = newProductTypeId;
                 return CreatedAtRoute("GetProductType", new { id = newProductTypeId }, value);
             }
+        }
 
+        // PUT producttype/PUT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int ProductTypeId, [FromBody] ProductType value)
+        {
+            string sql = $@"
+            UPDATE ProductType
+            SET Name = '{value.ProductTypeName}',
+            WHERE Id = {ProductTypeId}";
+
+            try
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    if (rowsAffected > 0)
+                    {
+                        return new StatusCodeResult(StatusCodes.Status204NoContent);
+                    }
+                    throw new Exception("No rows affected");
+                }
+            }
+            catch (Exception)
+            {
+                if (!ProductTypeExists(ProductTypeId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        //this boolean method checks to see if item already exists in DB, and it is needed for the PUT method
+        private bool ProductTypeExists(int id)
+        {
+            string sql = $"SELECT Id, ProductTypeName FROM ProductType WHERE Id = {id}";
+            using (IDbConnection conn = Connection)
+            {
+                return conn.Query<ProductType>(sql).Count() > 0;
+            }
         }
 
 
