edf168d50f229f90fe9df12094bc51b802770224
Shu52 <shusajid@gmail.com>
2018-09-18 11:01:53 -0500

    save before checkout to jw-computer-controller branch

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -41,19 +41,19 @@ namespace BangazonAPI.Controllers
         public async Task<IActionResult> GET(string q, string _include)
         {
             string sql = "SELECT * FROM Customer ";
-            if(_include == null)
-            {
-                _include = "";
-            }
-
-            if (q != null)
-            {
-                sql += $" WHERE 1-1 AND FirstName LIKE '%{q}%' OR LastName LIKE '%{q}%'";
-            }
             Console.WriteLine(sql);
 
             using (IDbConnection conn = Connection)
             {
+                if(_include == null)
+                {
+                    _include = "";
+                }
+
+                if (q != null)
+                {
+                    sql += $" WHERE 1=1 AND FirstName LIKE '%{q}%' OR LastName LIKE '%{q}%'";
+                }
 
                 if (_include != null && _include.Contains("payments"))
                 {
@@ -107,113 +107,110 @@ namespace BangazonAPI.Controllers
                 return Ok(customers);
 
             }
-                //    IEnumerable<Customer> customers = await conn.QueryAsync<Customer>(sql);
-                //    return Ok(customers);
-                //}
-                //}
+
 
                 // GET /customers/5
-                //    [HttpGet("{id}", Name = "GetCustomer")]
-                //    public async Task<IActionResult> Get([FromRoute]int id)
-                //    {
-                //        string sql = $"SELECT Id, Name, Language FROM Customer WHERE Id = {id}";
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public async Task<IActionResult> Get([FromRoute]int id)
+        {
+            string sql = $"SELECT Id, FirtName FROM Customer WHERE Id = {id}";
 
-                //        using (IDbConnection conn = Connection)
-                //        {
-                //            IEnumerable<Customer> customers = await conn.QueryAsync<Customer>(sql);
-                //            return Ok(customers);
-                //        }
-                //    }
-
-                //    // POST /customers
-                //    [HttpPost]
-                //    public async Task<IActionResult> Post([FromBody] Customer customer)
-                //    {
-                //        string sql = $@"INSERT INTO Customer
-                //        ()
-                //        VALUES
-                //        ();
-                //        select MAX(Id) from Customer;";
-
-                //        using (IDbConnection conn = Connection)
-                //        {
-                //            var customerId = (await conn.QueryAsync<int>(sql)).Single();
-                //            customer.Id = customerId;
-                //            return CreatedAtRoute("GetCustomer", new { id = customerId }, customer);
-                //        }
-                //    }
-
-                //    /*
-                //        PUT /customers/5
-                //        The [HttpPut] attribute ensures that this method will handle any
-                //        request to a `/customers/{id}` with the PUT HTTP verb. Alternatively,
-                //        I could name this method `PutCustomer`, or just `Put` and ASP.NET
-                //        will detect that the word `Put` is in the method name and ensure
-                //        that it will only be invoke for PUT operations.
-                //        All other controllers have this method named as `Put`. It's named
-                //        differently here to show that the [HttpPut] attribute enforces which
-                //        verb is handled.
-                //     */
-                //    [HttpPut("{id}")]
-                //    public async Task<IActionResult> ChangeCustomer(int id, [FromBody] Customer customer)
-                //    {
-                //        string sql = $@"
-                //        UPDATE Customer
-                //        SET '
-                //        WHERE Id = {id}";
-
-                //        try
-                //        {
-                //            using (IDbConnection conn = Connection)
-                //            {
-                //                int rowsAffected = await conn.ExecuteAsync(sql);
-                //                if (rowsAffected > 0)
-                //                {
-                //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
-                //                }
-                //                throw new Exception("No rows affected");
-                //            }
-                //        }
-                //        catch (Exception)
-                //        {
-                //            if (!CustomerExists(id))
-                //            {
-                //                return NotFound();
-                //            }
-                //            else
-                //            {
-                //                throw;
-                //            }
-                //        }
-                //    }
-
-                //    // DELETE /customers/5
-                //    [HttpDelete("{id}")]
-                //    public async Task<IActionResult> Delete(int id)
-                //    {
-                //        string sql = $@"DELETE FROM Customer WHERE Id = {id}";
-
-                //        using (IDbConnection conn = Connection)
-                //        {
-                //            int rowsAffected = await conn.ExecuteAsync(sql);
-                //            if (rowsAffected > 0)
-                //            {
-                //                return new StatusCodeResult(StatusCodes.Status204NoContent);
-                //            }
-                //            throw new Exception("No rows affected");
-                //        }
-
-                //    }
-
-                //    private bool CustomerExists(int id)
-                //    {
-                //        string sql = $"SELECT Id, Name, Language FROM Customer WHERE Id = {id}";
-                //        using (IDbConnection conn = Connection)
-                //        {
-                //            return conn.Query<Customer>(sql).Count() > 0;
-                //        }
-                //    }
-                //}
+            using (IDbConnection conn = Connection)
+            {
+                IEnumerable<Customer> customers = await conn.QueryAsync<Customer>(sql);
+                return Ok(customers);
             }
+        }
+
+        //    // POST /customers
+        //    [HttpPost]
+        //    public async Task<IActionResult> Post([FromBody] Customer customer)
+        //    {
+        //        string sql = $@"INSERT INTO Customer
+        //        ()
+        //        VALUES
+        //        ();
+        //        select MAX(Id) from Customer;";
+
+        //        using (IDbConnection conn = Connection)
+        //        {
+        //            var customerId = (await conn.QueryAsync<int>(sql)).Single();
+        //            customer.Id = customerId;
+        //            return CreatedAtRoute("GetCustomer", new { id = customerId }, customer);
+        //        }
+        //    }
+
+        //    /*
+        //        PUT /customers/5
+        //        The [HttpPut] attribute ensures that this method will handle any
+        //        request to a `/customers/{id}` with the PUT HTTP verb. Alternatively,
+        //        I could name this method `PutCustomer`, or just `Put` and ASP.NET
+        //        will detect that the word `Put` is in the method name and ensure
+        //        that it will only be invoke for PUT operations.
+        //        All other controllers have this method named as `Put`. It's named
+        //        differently here to show that the [HttpPut] attribute enforces which
+        //        verb is handled.
+        //     */
+        //    [HttpPut("{id}")]
+        //    public async Task<IActionResult> ChangeCustomer(int id, [FromBody] Customer customer)
+        //    {
+        //        string sql = $@"
+        //        UPDATE Customer
+        //        SET '
+        //        WHERE Id = {id}";
+
+        //        try
+        //        {
+        //            using (IDbConnection conn = Connection)
+        //            {
+        //                int rowsAffected = await conn.ExecuteAsync(sql);
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //        catch (Exception)
+        //        {
+        //            if (!CustomerExists(id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //    }
+
+        //    // DELETE /customers/5
+        //    [HttpDelete("{id}")]
+        //    public async Task<IActionResult> Delete(int id)
+        //    {
+        //        string sql = $@"DELETE FROM Customer WHERE Id = {id}";
+
+        //        using (IDbConnection conn = Connection)
+        //        {
+        //            int rowsAffected = await conn.ExecuteAsync(sql);
+        //            if (rowsAffected > 0)
+        //            {
+        //                return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //            }
+        //            throw new Exception("No rows affected");
+        //        }
+
+        //    }
+
+        //    private bool CustomerExists(int id)
+        //    {
+        //        string sql = $"SELECT Id, Name, Language FROM Customer WHERE Id = {id}";
+        //        using (IDbConnection conn = Connection)
+        //        {
+        //            return conn.Query<Customer>(sql).Count() > 0;
+        //        }
+        //    }
+        //}
+    }
     }
 }
